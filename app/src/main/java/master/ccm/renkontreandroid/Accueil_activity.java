b882aef3a310e9f0fe7d1a0be7d3070f6660fece
remig <urbaniec.remi@gmail.com>
2019-10-21 14:01:02 +0200

    Connexion et inscription Fin

--- app/src/main/java/master/ccm/renkontreandroid/Accueil_activity.java
+++ app/src/main/java/master/ccm/renkontreandroid/Accueil_activity.java
@@ -3,12 +3,29 @@ package master.ccm.renkontreandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Accueil_activity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
+    private TextView tv_mail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_accueil);
+
+        mAuth = FirebaseAuth.getInstance();
+        tv_mail=findViewById(R.id.id_mail);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            tv_mail.setText(email);
+        }
     }
 }
--- app/src/main/java/master/ccm/renkontreandroid/Connexion_activity.java
+++ app/src/main/java/master/ccm/renkontreandroid/Connexion_activity.java
@@ -1,14 +1,84 @@
 package master.ccm.renkontreandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Connexion_activity extends AppCompatActivity {
+
+    // déclaration de l'instande FirebaseAuth
+    private FirebaseAuth mAuth;
+
+    private EditText champEmail;
+    private EditText champMDP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_connexion);
+
+        // initialisation de la variable Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        champEmail=findViewById(R.id.Et_mail_connexion);
+        champMDP=findViewById(R.id.Et_password_connexion);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        Intent intent = new Intent(this, Accueil_activity.class);
+        startActivity(intent);
+        finish();
+    }
+    private void connexionFirebase(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("connexion Firebase", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("connexion Firebase", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Connexion_activity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+    public void onClickInscription(View view) {
+        Intent intent = new Intent(this, Inscription_activity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onClickSignIn(View view) {
+        String email = champEmail.getText().toString();
+        String password = champMDP.getText().toString();
+        connexionFirebase(email,password);
     }
 }
