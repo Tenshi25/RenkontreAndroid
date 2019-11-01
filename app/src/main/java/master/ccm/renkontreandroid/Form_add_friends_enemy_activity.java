bf55fbe463f345fc0645df332a833baebfc64cc7
remig <urbaniec.remi@gmail.com>
2019-11-01 17:00:28 +0100

    ajout Ami / ennemi +preparation de l'interface List Ami / Ennemi

---
+++ app/src/main/java/master/ccm/renkontreandroid/Form_add_friends_enemy_activity.java
@@ -0,0 +1,14 @@
+package master.ccm.renkontreandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_add_friends_enemy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_add_friends_enemy);
+    }
+}
---
+++ app/src/main/java/master/ccm/renkontreandroid/ListFriendsEnemy_activity.java
@@ -0,0 +1,14 @@
+package master.ccm.renkontreandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListFriendsEnemy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_friends_enemy);
+    }
+}
